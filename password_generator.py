f82549e0e413777afe0c5388aa47b587982fe502
Tosin Babatunde <obatos2105@gmail.com>
2022-09-24 15:59:16 -0400

    we modified the code here a little

--- password_generator.py
+++ password_generator.py
@@ -4,7 +4,7 @@ chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*?0123456789
 
 print('Welcome To Your Password Generator')
 number = int(input("How many passwords would you like to generate? "))
-length = int(input("Enter a password length? "))
+length = int(input("enter a password length? "))
 
 print("\nHere are your passwords:")
 for pwd in range(number):
